be9fcf673ff7041a3267a262bc195ae28f299b86
Pavel1 <pavel.pervyhin@gmail.com>
2026-01-19 01:15:21 +0100

    Third commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print ("Hello World")
+def main():
+    print("Hello World")
+    print("Hello Git")
+    print("Hi dear Friend")
 
-print ("Hello Git")
-
-print ("Hi dear Friend")
-
+if __name__ == "__main__":
+    main()
